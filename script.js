aa4b23c98d634e0cc31fd2acbb8ded1b88558afe
ArunKumar <beta.a7681k@gmail.com>
2025-11-09 03:56:20 +0530

    Add toggle button to change read status

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const myLibrary = [
-{id: 'a533dbc1-47bb-4af9-819f-36d37c863861', title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 310, read: 'not read'},
-{id: '84e57aee-4332-4343-8d44-e3643ebf6496', title: '1984', author: 'George Orwell', pages: 328, read: 'read'},
-{id: 'edce4fbe-04b1-4af6-bda5-8231ee35da2a', title: 'Pride and Prejudice', author: 'Jane Austen', pages: 279, read: 'read'},
-{id: '22525267-a395-47eb-920f-4e4b2bc45e2a', title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 324, read: 'not read'}
+  new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'not read'),
+  new Book('1984', 'George Orwell', 328, 'read'),
+  new Book('Pride and Prejudice', 'Jane Austen', 279, 'read'),
+  new Book('To Kill a Mockingbird', 'Harper Lee', 324, 'not read')
 ];
 
 function Book(title, author, pages, read) {
@@ -19,6 +19,10 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function() {
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
+
+Book.prototype.toggleStatus = function() {
+    this.read = this.read === 'read' ? 'not read' : 'read';
+}
 
 function addBookToLibrary(title, author, pages, read) {
     const books = new Book (title, author, pages, read);
@@ -51,8 +55,8 @@ function dynamicBookAddition() {
         pages.classList.add("pages");
         pages.textContent = `Pages: ${item.pages}`;
         
-        const status = document.createElement("p")
-        status.classList.add("status");
+        const status = document.createElement("button")
+        status.classList.add(item.read === 'read' ? 'read' : 'not-read');
         status.textContent = `Status: ${item.read}`;
 
         content.append(removeBtn, title, author, pages, status);
@@ -75,7 +79,7 @@ form.addEventListener("submit", (e)=> {
     const title = document.querySelector("#title-input").value.trim();
     const author = document.querySelector("#author-input").value.trim();
     const pages = parseInt(document.querySelector("#pages-input").value);
-    const status = document.querySelector("#status-input").value.trim();
+    const status = document.querySelector('input[name="status"]:checked').value;
 
     if (!title || !author || !pages || !status) {
     alert("Please fill in all fields!");
@@ -91,11 +95,23 @@ const closeBtn = document.querySelector("#close-btn")
 closeBtn.addEventListener("click", ()=> dialog.close());
 
 booksDisplayContainer.addEventListener("click", (e)=> {
-    if(e.target.classList.contains("removeBtn")) {
+    if(e.target.classList.contains("removeBtn")) {   //remove button
         const removeCard = e.target.closest(".cards");
 
-        const removeCardId = myLibrary.findIndex(item => item.id = removeCard.dataset.id);
+        const removeCardId = myLibrary.findIndex(item => item.id === removeCard.dataset.id);
         myLibrary.splice(removeCardId, 1);
         removeCard.remove();
+    }
+    if(e.target.classList.contains("read") || e.target.classList.contains("not-read")) {       //toggle button
+        const toggle = e.target.closest(".cards");
+        const toggleBookID = toggle.dataset.id;
+
+        const toggleBook = myLibrary.find(item => item.id === toggleBookID);
+        toggleBook.toggleStatus();
+        e.target.textContent = `Status: ${toggleBook.read}`;
+
+        e.target.classList.remove("read", "not-read");
+
+        e.target.classList.add(toggleBook.read === "read" ? "read" : "not-read");
     }
 })
